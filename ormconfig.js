b10670c36b7b6430258caf145102990aa293f273
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-09-02 09:28:59 -0300

    update/cleanup configs

--- ormconfig.js
+++ ormconfig.js
@@ -8,9 +8,9 @@ module.exports = {
   name: 'default',
   type: process.env.DB_TYPE,
   database: process.env.DB_DATABASE,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
   url: process.env.DB_URL,
   migrations: ['src/database/migrations/*.ts', 'dist/database/migrations/*.js'],
   entities: ['dist/entities/*.js', 'src/entities/*.ts'],
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const API_PORT = process.env.PORT || 3000;
 
 api.listen(API_PORT, () => {
   console.log('server running');
-  console.log('migrations', process.env.MIGRATIONS_FOLDER);
-  console.log('entities', process.env.ENTITIES_FOLDER);
-  console.log('path/url', process.env.DB_PATH);
+  // console.log('migrations', process.env.MIGRATIONS_FOLDER);
+  // console.log('entities', process.env.ENTITIES_FOLDER);
+  // console.log('path/url', process.env.DB_PATH);
 });
